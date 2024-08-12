4ad6293a0aff73160f4c24b1e93309e40bede070
jimjiang2 <jimjiang4@163.com>
2024-08-12 22:39:33 +0800

    Add files via upload
    ADD driver with ch341a

--- adafruit_si5351.py
+++ adafruit_si5351.py
@@ -13,100 +13,99 @@ library at: https://github.com/adafruit/Adafruit_Si5351_Library/
 * Author(s): Tony DiCola
 """
 import math
+#from micropython import const
+#from D:\PMIC\FPGA\py_pjs\py_pmic_sample_test\
+from ch341dll_32bits_wrap.ch341dll_i2c_for_si5351 import *
 
-from micropython import const
-
-from adafruit_bus_device import i2c_device
-
-try:
-    import typing  # pylint: disable=unused-import
-    from busio import I2C
-except ImportError:
-    pass
+#try:
+#    import typing  # pylint: disable=unused-import
+#    from busio import I2C
+#except ImportError:
+#    pass
 
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_SI5351.git"
 
 
 # Internal constants:
-_SI5351_ADDRESS = const(0x60)  # Assumes ADDR pin = low
-_SI5351_READBIT = const(0x01)
+_SI5351_ADDRESS = 0x60  # Assumes ADDR pin = low
+_SI5351_READBIT = 0x01
 _SI5351_CRYSTAL_FREQUENCY = 25000000.0  # Fixed 25mhz crystal on board.
-_SI5351_REGISTER_0_DEVICE_STATUS = const(0)
-_SI5351_REGISTER_1_INTERRUPT_STATUS_STICKY = const(1)
-_SI5351_REGISTER_2_INTERRUPT_STATUS_MASK = const(2)
-_SI5351_REGISTER_3_OUTPUT_ENABLE_CONTROL = const(3)
-_SI5351_REGISTER_9_OEB_PIN_ENABLE_CONTROL = const(9)
-_SI5351_REGISTER_15_PLL_INPUT_SOURCE = const(15)
-_SI5351_REGISTER_16_CLK0_CONTROL = const(16)
-_SI5351_REGISTER_17_CLK1_CONTROL = const(17)
-_SI5351_REGISTER_18_CLK2_CONTROL = const(18)
-_SI5351_REGISTER_19_CLK3_CONTROL = const(19)
-_SI5351_REGISTER_20_CLK4_CONTROL = const(20)
-_SI5351_REGISTER_21_CLK5_CONTROL = const(21)
-_SI5351_REGISTER_22_CLK6_CONTROL = const(22)
-_SI5351_REGISTER_23_CLK7_CONTROL = const(23)
-_SI5351_REGISTER_24_CLK3_0_DISABLE_STATE = const(24)
-_SI5351_REGISTER_25_CLK7_4_DISABLE_STATE = const(25)
-_SI5351_REGISTER_42_MULTISYNTH0_PARAMETERS_1 = const(42)
-_SI5351_REGISTER_43_MULTISYNTH0_PARAMETERS_2 = const(43)
-_SI5351_REGISTER_44_MULTISYNTH0_PARAMETERS_3 = const(44)
-_SI5351_REGISTER_45_MULTISYNTH0_PARAMETERS_4 = const(45)
-_SI5351_REGISTER_46_MULTISYNTH0_PARAMETERS_5 = const(46)
-_SI5351_REGISTER_47_MULTISYNTH0_PARAMETERS_6 = const(47)
-_SI5351_REGISTER_48_MULTISYNTH0_PARAMETERS_7 = const(48)
-_SI5351_REGISTER_49_MULTISYNTH0_PARAMETERS_8 = const(49)
-_SI5351_REGISTER_50_MULTISYNTH1_PARAMETERS_1 = const(50)
-_SI5351_REGISTER_51_MULTISYNTH1_PARAMETERS_2 = const(51)
-_SI5351_REGISTER_52_MULTISYNTH1_PARAMETERS_3 = const(52)
-_SI5351_REGISTER_53_MULTISYNTH1_PARAMETERS_4 = const(53)
-_SI5351_REGISTER_54_MULTISYNTH1_PARAMETERS_5 = const(54)
-_SI5351_REGISTER_55_MULTISYNTH1_PARAMETERS_6 = const(55)
-_SI5351_REGISTER_56_MULTISYNTH1_PARAMETERS_7 = const(56)
-_SI5351_REGISTER_57_MULTISYNTH1_PARAMETERS_8 = const(57)
-_SI5351_REGISTER_58_MULTISYNTH2_PARAMETERS_1 = const(58)
-_SI5351_REGISTER_59_MULTISYNTH2_PARAMETERS_2 = const(59)
-_SI5351_REGISTER_60_MULTISYNTH2_PARAMETERS_3 = const(60)
-_SI5351_REGISTER_61_MULTISYNTH2_PARAMETERS_4 = const(61)
-_SI5351_REGISTER_62_MULTISYNTH2_PARAMETERS_5 = const(62)
-_SI5351_REGISTER_63_MULTISYNTH2_PARAMETERS_6 = const(63)
-_SI5351_REGISTER_64_MULTISYNTH2_PARAMETERS_7 = const(64)
-_SI5351_REGISTER_65_MULTISYNTH2_PARAMETERS_8 = const(65)
-_SI5351_REGISTER_66_MULTISYNTH3_PARAMETERS_1 = const(66)
-_SI5351_REGISTER_67_MULTISYNTH3_PARAMETERS_2 = const(67)
-_SI5351_REGISTER_68_MULTISYNTH3_PARAMETERS_3 = const(68)
-_SI5351_REGISTER_69_MULTISYNTH3_PARAMETERS_4 = const(69)
-_SI5351_REGISTER_70_MULTISYNTH3_PARAMETERS_5 = const(70)
-_SI5351_REGISTER_71_MULTISYNTH3_PARAMETERS_6 = const(71)
-_SI5351_REGISTER_72_MULTISYNTH3_PARAMETERS_7 = const(72)
-_SI5351_REGISTER_73_MULTISYNTH3_PARAMETERS_8 = const(73)
-_SI5351_REGISTER_74_MULTISYNTH4_PARAMETERS_1 = const(74)
-_SI5351_REGISTER_75_MULTISYNTH4_PARAMETERS_2 = const(75)
-_SI5351_REGISTER_76_MULTISYNTH4_PARAMETERS_3 = const(76)
-_SI5351_REGISTER_77_MULTISYNTH4_PARAMETERS_4 = const(77)
-_SI5351_REGISTER_78_MULTISYNTH4_PARAMETERS_5 = const(78)
-_SI5351_REGISTER_79_MULTISYNTH4_PARAMETERS_6 = const(79)
-_SI5351_REGISTER_80_MULTISYNTH4_PARAMETERS_7 = const(80)
-_SI5351_REGISTER_81_MULTISYNTH4_PARAMETERS_8 = const(81)
-_SI5351_REGISTER_82_MULTISYNTH5_PARAMETERS_1 = const(82)
-_SI5351_REGISTER_83_MULTISYNTH5_PARAMETERS_2 = const(83)
-_SI5351_REGISTER_84_MULTISYNTH5_PARAMETERS_3 = const(84)
-_SI5351_REGISTER_85_MULTISYNTH5_PARAMETERS_4 = const(85)
-_SI5351_REGISTER_86_MULTISYNTH5_PARAMETERS_5 = const(86)
-_SI5351_REGISTER_87_MULTISYNTH5_PARAMETERS_6 = const(87)
-_SI5351_REGISTER_88_MULTISYNTH5_PARAMETERS_7 = const(88)
-_SI5351_REGISTER_89_MULTISYNTH5_PARAMETERS_8 = const(89)
-_SI5351_REGISTER_90_MULTISYNTH6_PARAMETERS = const(90)
-_SI5351_REGISTER_91_MULTISYNTH7_PARAMETERS = const(91)
-_SI5351_REGISTER_092_CLOCK_6_7_OUTPUT_DIVIDER = const(92)
-_SI5351_REGISTER_165_CLK0_INITIAL_PHASE_OFFSET = const(165)
-_SI5351_REGISTER_166_CLK1_INITIAL_PHASE_OFFSET = const(166)
-_SI5351_REGISTER_167_CLK2_INITIAL_PHASE_OFFSET = const(167)
-_SI5351_REGISTER_168_CLK3_INITIAL_PHASE_OFFSET = const(168)
-_SI5351_REGISTER_169_CLK4_INITIAL_PHASE_OFFSET = const(169)
-_SI5351_REGISTER_170_CLK5_INITIAL_PHASE_OFFSET = const(170)
-_SI5351_REGISTER_177_PLL_RESET = const(177)
-_SI5351_REGISTER_183_CRYSTAL_INTERNAL_LOAD_CAPACITANCE = const(183)
+_SI5351_REGISTER_0_DEVICE_STATUS = 0
+_SI5351_REGISTER_1_INTERRUPT_STATUS_STICKY = 1
+_SI5351_REGISTER_2_INTERRUPT_STATUS_MASK = 2
+_SI5351_REGISTER_3_OUTPUT_ENABLE_CONTROL = 3
+_SI5351_REGISTER_9_OEB_PIN_ENABLE_CONTROL = 9
+_SI5351_REGISTER_15_PLL_INPUT_SOURCE = 15
+_SI5351_REGISTER_16_CLK0_CONTROL = 16
+_SI5351_REGISTER_17_CLK1_CONTROL = 17
+_SI5351_REGISTER_18_CLK2_CONTROL = 18
+_SI5351_REGISTER_19_CLK3_CONTROL = 19
+_SI5351_REGISTER_20_CLK4_CONTROL = 20
+_SI5351_REGISTER_21_CLK5_CONTROL = 21
+_SI5351_REGISTER_22_CLK6_CONTROL = 22
+_SI5351_REGISTER_23_CLK7_CONTROL = 23
+_SI5351_REGISTER_24_CLK3_0_DISABLE_STATE = 24
+_SI5351_REGISTER_25_CLK7_4_DISABLE_STATE = 25
+_SI5351_REGISTER_42_MULTISYNTH0_PARAMETERS_1 = 42
+_SI5351_REGISTER_43_MULTISYNTH0_PARAMETERS_2 = 43
+_SI5351_REGISTER_44_MULTISYNTH0_PARAMETERS_3 = 44
+_SI5351_REGISTER_45_MULTISYNTH0_PARAMETERS_4 = 45
+_SI5351_REGISTER_46_MULTISYNTH0_PARAMETERS_5 = 46
+_SI5351_REGISTER_47_MULTISYNTH0_PARAMETERS_6 = 47
+_SI5351_REGISTER_48_MULTISYNTH0_PARAMETERS_7 = 48
+_SI5351_REGISTER_49_MULTISYNTH0_PARAMETERS_8 = 49
+_SI5351_REGISTER_50_MULTISYNTH1_PARAMETERS_1 = 50
+_SI5351_REGISTER_51_MULTISYNTH1_PARAMETERS_2 = 51
+_SI5351_REGISTER_52_MULTISYNTH1_PARAMETERS_3 = 52
+_SI5351_REGISTER_53_MULTISYNTH1_PARAMETERS_4 = 53
+_SI5351_REGISTER_54_MULTISYNTH1_PARAMETERS_5 = 54
+_SI5351_REGISTER_55_MULTISYNTH1_PARAMETERS_6 = 55
+_SI5351_REGISTER_56_MULTISYNTH1_PARAMETERS_7 = 56
+_SI5351_REGISTER_57_MULTISYNTH1_PARAMETERS_8 = 57
+_SI5351_REGISTER_58_MULTISYNTH2_PARAMETERS_1 = 58
+_SI5351_REGISTER_59_MULTISYNTH2_PARAMETERS_2 = 59
+_SI5351_REGISTER_60_MULTISYNTH2_PARAMETERS_3 = 60
+_SI5351_REGISTER_61_MULTISYNTH2_PARAMETERS_4 = 61
+_SI5351_REGISTER_62_MULTISYNTH2_PARAMETERS_5 = 62
+_SI5351_REGISTER_63_MULTISYNTH2_PARAMETERS_6 = 63
+_SI5351_REGISTER_64_MULTISYNTH2_PARAMETERS_7 = 64
+_SI5351_REGISTER_65_MULTISYNTH2_PARAMETERS_8 = 65
+_SI5351_REGISTER_66_MULTISYNTH3_PARAMETERS_1 = 66
+_SI5351_REGISTER_67_MULTISYNTH3_PARAMETERS_2 = 67
+_SI5351_REGISTER_68_MULTISYNTH3_PARAMETERS_3 = 68
+_SI5351_REGISTER_69_MULTISYNTH3_PARAMETERS_4 = 69
+_SI5351_REGISTER_70_MULTISYNTH3_PARAMETERS_5 = 70
+_SI5351_REGISTER_71_MULTISYNTH3_PARAMETERS_6 = 71
+_SI5351_REGISTER_72_MULTISYNTH3_PARAMETERS_7 = 72
+_SI5351_REGISTER_73_MULTISYNTH3_PARAMETERS_8 = 73
+_SI5351_REGISTER_74_MULTISYNTH4_PARAMETERS_1 = 74
+_SI5351_REGISTER_75_MULTISYNTH4_PARAMETERS_2 = 75
+_SI5351_REGISTER_76_MULTISYNTH4_PARAMETERS_3 = 76
+_SI5351_REGISTER_77_MULTISYNTH4_PARAMETERS_4 = 77
+_SI5351_REGISTER_78_MULTISYNTH4_PARAMETERS_5 = 78
+_SI5351_REGISTER_79_MULTISYNTH4_PARAMETERS_6 = 79
+_SI5351_REGISTER_80_MULTISYNTH4_PARAMETERS_7 = 80
+_SI5351_REGISTER_81_MULTISYNTH4_PARAMETERS_8 = 81
+_SI5351_REGISTER_82_MULTISYNTH5_PARAMETERS_1 = 82
+_SI5351_REGISTER_83_MULTISYNTH5_PARAMETERS_2 = 83
+_SI5351_REGISTER_84_MULTISYNTH5_PARAMETERS_3 = 84
+_SI5351_REGISTER_85_MULTISYNTH5_PARAMETERS_4 = 85
+_SI5351_REGISTER_86_MULTISYNTH5_PARAMETERS_5 = 86
+_SI5351_REGISTER_87_MULTISYNTH5_PARAMETERS_6 = 87
+_SI5351_REGISTER_88_MULTISYNTH5_PARAMETERS_7 = 88
+_SI5351_REGISTER_89_MULTISYNTH5_PARAMETERS_8 = 89
+_SI5351_REGISTER_90_MULTISYNTH6_PARAMETERS = 90
+_SI5351_REGISTER_91_MULTISYNTH7_PARAMETERS = 91
+_SI5351_REGISTER_092_CLOCK_6_7_OUTPUT_DIVIDER = 92
+_SI5351_REGISTER_165_CLK0_INITIAL_PHASE_OFFSET = 165
+_SI5351_REGISTER_166_CLK1_INITIAL_PHASE_OFFSET = 166
+_SI5351_REGISTER_167_CLK2_INITIAL_PHASE_OFFSET = 167
+_SI5351_REGISTER_168_CLK3_INITIAL_PHASE_OFFSET = 168
+_SI5351_REGISTER_169_CLK4_INITIAL_PHASE_OFFSET = 169
+_SI5351_REGISTER_170_CLK5_INITIAL_PHASE_OFFSET = 170
+_SI5351_REGISTER_177_PLL_RESET = 177
+_SI5351_REGISTER_183_CRYSTAL_INTERNAL_LOAD_CAPACITANCE = 183
 
 # User-facing constants:
 R_DIV_1 = 0
@@ -161,18 +160,18 @@ class SI5351:
 
         def _configure_registers(self, p1: int, p2: int, p3: int) -> None:
             # Update PLL registers.
-            with self._si5351._device as i2c:
-                buf = self._si5351._BUFFER
-                buf[0] = self._base
-                buf[1] = (p3 & 0x0000FF00) >> 8
-                buf[2] = p3 & 0x000000FF
-                buf[3] = (p1 & 0x00030000) >> 16
-                buf[4] = (p1 & 0x0000FF00) >> 8
-                buf[5] = p1 & 0x000000FF
-                buf[6] = ((p3 & 0x000F0000) >> 12) | ((p2 & 0x000F0000) >> 16)
-                buf[7] = (p2 & 0x0000FF00) >> 8
-                buf[8] = p2 & 0x000000FF
-                i2c.write(buf, end=9)
+            i2c = self._si5351._device
+            buf = self._si5351._BUFFER
+            buf[0] = self._base
+            buf[1] = (p3 & 0x0000FF00) >> 8
+            buf[2] = p3 & 0x000000FF
+            buf[3] = (p1 & 0x00030000) >> 16
+            buf[4] = (p1 & 0x0000FF00) >> 8
+            buf[5] = p1 & 0x000000FF
+            buf[6] = ((p3 & 0x000F0000) >> 12) | ((p2 & 0x000F0000) >> 16)
+            buf[7] = (p2 & 0x0000FF00) >> 8
+            buf[8] = p2 & 0x000000FF
+            i2c.write(buf, end=9)
             # Reset both PLLs.
             self._si5351._write_u8(_SI5351_REGISTER_177_PLL_RESET, (1 << 7) | (1 << 5))
 
@@ -312,18 +311,18 @@ class SI5351:
 
         def _configure_registers(self, p1: int, p2: int, p3: int) -> None:
             # Update MSx registers.
-            with self._si5351._device as i2c:
-                buf = self._si5351._BUFFER
-                buf[0] = self._base
-                buf[1] = (p3 & 0x0000FF00) >> 8
-                buf[2] = p3 & 0x000000FF
-                buf[3] = (p1 & 0x00030000) >> 16
-                buf[4] = (p1 & 0x0000FF00) >> 8
-                buf[5] = p1 & 0x000000FF
-                buf[6] = ((p3 & 0x000F0000) >> 12) | ((p2 & 0x000F0000) >> 16)
-                buf[7] = (p2 & 0x0000FF00) >> 8
-                buf[8] = p2 & 0x000000FF
-                i2c.write(buf, end=9)
+            i2c  = self._si5351._device ;
+            buf = self._si5351._BUFFER
+            buf[0] = self._base
+            buf[1] = (p3 & 0x0000FF00) >> 8
+            buf[2] = p3 & 0x000000FF
+            buf[3] = (p1 & 0x00030000) >> 16
+            buf[4] = (p1 & 0x0000FF00) >> 8
+            buf[5] = p1 & 0x000000FF
+            buf[6] = ((p3 & 0x000F0000) >> 12) | ((p2 & 0x000F0000) >> 16)
+            buf[7] = (p2 & 0x0000FF00) >> 8
+            buf[8] = p2 & 0x000000FF
+            i2c.write(buf, end=9)
 
         def configure_integer(
             self, pll: "PLL", divider: int, inverted: bool = False
@@ -411,19 +410,18 @@ class SI5351:
     # This is not thread-safe or re-entrant by design!
     _BUFFER = bytearray(9)
 
-    def __init__(self, i2c: I2C, *, address: int = _SI5351_ADDRESS) -> None:
-        self._device = i2c_device.I2CDevice(i2c, address)
+    def __init__(self, i2c_hd, *, address: int = _SI5351_ADDRESS) -> None:
+        self._device = i2c_hd;#//i2c_device.I2CDevice(i2c, address)
         # Setup the SI5351.
         # Disable all outputs setting CLKx_DIS high.
         self._write_u8(_SI5351_REGISTER_3_OUTPUT_ENABLE_CONTROL, 0xFF)
         # Power down all output drivers
         # Class-level buffer to reduce allocations and heap fragmentation.
         # This is not thread-safe or re-entrant by design!
-        with self._device as i2c_bus:
-            self._BUFFER[0] = _SI5351_REGISTER_16_CLK0_CONTROL
-            for i in range(1, 9):
-                self._BUFFER[i] = 0x80
-            i2c_bus.write(self._BUFFER, end=9)
+        self._BUFFER[0] = _SI5351_REGISTER_16_CLK0_CONTROL
+        for i in range(1, 9):
+            self._BUFFER[i] = 0x80
+        self._device.write(self._BUFFER, end=9)
         # Initialize PLL A and B objects.
         self.pll_a = self._PLL(self, 26, 0)
         self.pll_b = self._PLL(self, 34, (1 << 5))
@@ -449,18 +447,18 @@ class SI5351:
 
     def _read_u8(self, address: int) -> int:
         # Read an 8-bit unsigned value from the specified 8-bit address.
-        with self._device as i2c:
-            self._BUFFER[0] = address & 0xFF
-            i2c.write(self._BUFFER, end=1)
-            i2c.readinto(self._BUFFER, end=1)
+        i2c= self._device ;#//as i2c:
+        self._BUFFER[0] = address & 0xFF
+           #i2c.write(self._BUFFER, end=1)
+        i2c.ch341_sri2c(self._device.i2c_addr,  address )
         return self._BUFFER[0]
 
     def _write_u8(self, address: int, val: int) -> None:
         # Write an 8-bit unsigned value to the specified 8-bit address.
-        with self._device as i2c:
+#            self._device
             self._BUFFER[0] = address & 0xFF
             self._BUFFER[1] = val & 0xFF
-            i2c.write(self._BUFFER, end=2)
+            self._device.write(self._BUFFER, end=2)
 
     @property
     def outputs_enabled(self) -> bool:
